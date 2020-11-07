3d9c480a5e731c51d42ba4332fb3b6a4d0b42a07
ahacad <1393345075@qq.com>
2020-11-07 21:45:54 +0800

    feat(pdf-card): add pdfcard

--- src/Pdfcard.jsx
+++ src/Pdfcard.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import PropTypes from 'prop-types';
+import './Pdfcard.scss';
 
-export default function Pdfcard() {
+const options = {
+  cMapUrl: 'cmaps/',
+  cMapPacked: true,
+};
+
+export default function Pdfcard(props) {
+  const { pdf, pageNumber } = props;
+
   return (
     <div className="pdfcard">
       <div className="pdfcard__cover">
-        DOCUMEMNT
+        <Document
+          file={pdf}
+          options={options}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
       </div>
       <div className="pdfcard__bookname">
         Unix programming philosophy
@@ -12,3 +28,13 @@ export default function Pdfcard() {
     </div>
   );
 }
+
+Pdfcard.defaultProps = {
+  pdf: '',
+  pageNumber: 1,
+};
+
+Pdfcard.propTypes = {
+  pdf: PropTypes.string,
+  pageNumber: PropTypes.number,
+};

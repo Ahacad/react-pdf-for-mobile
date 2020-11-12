03b3b489dca05af778a07154d350f2bb3f5ed2ab
ahacad <1393345075@qq.com>
2020-11-12 20:25:50 +0800

    feat(books): generated books.json using nodejs

---
+++ src/getBookPath.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+let books = {};
+
+function walkDir(dir, callback) {
+  fs.readdirSync(dir).forEach((f) => {
+    const dirPath = path.join(dir, f);
+    const isDirectory = fs.statSync(dirPath).isDirectory();
+    if (isDirectory) {
+      walkDir(dirPath, callback);
+    } else {
+      callback(dirPath);
+    }
+  });
+}
+
+walkDir('./books', (filePath) => {
+  if (books[filePath.split("/")[1]] === undefined) {
+    books[filePath.split("/")[1]] = [];
+  }
+  books[filePath.split("/")[1]].push(filePath);
+});
+
+fs.writeFileSync('books.json', JSON.stringify(books, null, '  '));

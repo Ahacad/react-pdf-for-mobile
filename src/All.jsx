85bbd2de0be4d0294cf607a9f223eed3c07816eb
ahacad <1393345075@qq.com>
2020-11-18 21:25:31 +0800

    :construction: refactory

--- src/All.jsx
+++ src/All.jsx
@@ -1,63 +1,74 @@
 import React from 'react';
 // import Sample from './Sample';
 // import Pdfcard from './Pdfcard';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useRouteMatch,
-  useParams,
-} from 'react-router-dom';
+//import {
+  //BrowserRouter as Router,
+  //Switch,
+  //Route,
+  //Link,
+  //useRouteMatch,
+  //useParams,
+//} from 'react-router-dom';
 import ScrollablePdf from './ScrollablePdf';
-import books from './books.json';
 
-const keys = Object.keys(books);
+function importall(r) {
+  let pdfbooks = {};
+  r.keys().map((item, index) => { pdfbooks[item.replace('../books', '')] = r(item); });
+  return pdfbooks;
+}
+const pdfbooks = importall(require.context('../books', true, /\.pdf$/));
+
+import books from './books.json';
+const categories = Object.keys(books);
 const values = Object.values(books);
+
+const tmp = Object.keys(pdfbooks);
+const tmp1 = Object.values(pdfbooks);
 
 export default function All() {
   return (
     <div>
-      <Router>
-        <ul>
-          {keys.map((key) => (
-            <li><Link to={`/${key}`}>{key}</Link></li>
-          ))}
-        </ul>
-        <hr />
-        <Switch>
-          {keys.map((key, index) => (
-            <Route path={`/${key}`}>
-              <Category index={index} />
-            </Route>
-          ))}
-        </Switch>
-      </Router>
+      <ScrollablePdf pdf={tmp1[1]} />
     </div>
   );
 }
+      //<Router>
+        //<ul>
+          //{keys.map((key) => (
+            //<li><Link to={`/${key}`}>{key}</Link></li>
+          //))}
+        //</ul>
+        //<hr />
+        //<Switch>
+          //{keys.map((key, index) => (
+            //<Route path={`/${key}`}>
+              //<Category index={index} />
+            //</Route>
+          //))}
+        //</Switch>
+      //</Router>
 
-function Category(props) {
-  const { path, url } = useRouteMatch();
-  return (
-    <div>
-      <ul>
-        {values[props.index].map((element) => (
-          <li><Link to={`${url}/${element}`}>{ element }</Link></li>
-        ))}
-      </ul>
-      <Switch>
-      </Switch>
-    </div>
-  );
-}
+//function Category(props) {
+  //const { path, url } = useRouteMatch();
+  //return (
+    //<div>
+      //<ul>
+        //{values[props.index].map((element) => (
+          //<li><Link to={`${url}/${element}`}>{ element }</Link></li>
+        //))}
+      //</ul>
+      //<Switch>
+      //</Switch>
+    //</div>
+  //);
+//}
 
-function Onebook(props) {
-  let { topicId } = useParams();
+//function Onebook(props) {
+  //let { topicId } = useParams();
 
-  return (
-    <div>
-      <ScrollablePdf pdf={props.pdf} />
-    </div>
-  );
-}
+  //return (
+    //<div>
+      //<ScrollablePdf pdf={props.pdf} />
+    //</div>
+  //);
+//}
